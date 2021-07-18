f2c60287e024eb7fcc5190436b7c0fe15ea69b5f
= <=>
2021-07-18 05:16:33 +0000

    updates for prism

--- src/buildList.js
+++ src/buildList.js
@@ -1,14 +1,12 @@
 const { version } = require("../package.json");
-const mainnet = require("./tokens/mainnet.json");
-const ropsten = require("./tokens/ropsten.json");
-const rinkeby = require("./tokens/rinkeby.json");
-const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");
+const eth = require("./tokens/eth.json");
+const bsc = require("./tokens/bsc.json");
+const bsctestnet = require("./tokens/bsctestnet.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
   return {
-    name: "Uniswap Default List",
+    name: "Empire Default List",
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
@@ -17,8 +15,8 @@ module.exports = function buildList() {
     },
     tags: {},
     logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
-    keywords: ["uniswap", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby]
+    keywords: ["empire", "default"],
+    tokens: [...eth, ...bsc, ...bsctestnet]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
