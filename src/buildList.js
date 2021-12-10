f96cdd2e3061166e9d0c8ed3221f4075746d3620
Moody Salem <moody.salem@gmail.com>
2021-12-10 13:14:32 -0500

    rename list

--- src/buildList.js
+++ src/buildList.js
@@ -8,7 +8,7 @@ const kovan = require("./tokens/kovan.json");
 module.exports = function buildList() {
   const parsed = version.split(".");
   return {
-    name: "Uniswap Default List",
+    name: "Uniswap Labs List",
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
