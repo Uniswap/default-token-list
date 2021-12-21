8f8ed99e8bfb507ab2b24c87aad05496df316af3
Will Hennessy <hennessywill@gmail.com>
2021-12-21 17:15:52 -0600

    Add polygon tokens (#1026)
    * Add polygon tokens
    
    * Delete package-lock
    
    * Add WETH to polygon and mumbai
    
    * Add MATIC to mainnet, WMATIC to polygon and mumbai

--- src/buildList.js
+++ src/buildList.js
@@ -4,6 +4,8 @@ const ropsten = require("./tokens/ropsten.json");
 const rinkeby = require("./tokens/rinkeby.json");
 const goerli = require("./tokens/goerli.json");
 const kovan = require("./tokens/kovan.json");
+const polygon = require("./tokens/polygon.json");
+const mumbai = require("./tokens/mumbai.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
@@ -18,7 +20,7 @@ module.exports = function buildList() {
     tags: {},
     logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
     keywords: ["uniswap", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby]
+    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...polygon, ...mumbai]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
