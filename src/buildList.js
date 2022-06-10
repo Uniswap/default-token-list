7f8843cc4a670f1a3107efe3ef6e930f45bb6349
0x369D <74992597+0x369D@users.noreply.github.com>
2022-06-10 12:11:12 +1200

    add kava list

--- src/buildList.js
+++ src/buildList.js
@@ -7,6 +7,7 @@ const matic = require("./tokens/matic.json");
 const xdai = require("./tokens/xdai.json");
 const cro = require("./tokens/cro.json");
 const harmony = require("./tokens/harmony.json");
+const kava = require("./tokens/kava.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
@@ -21,7 +22,7 @@ module.exports = function buildList() {
     tags: {},
     logoURI: "https://raw.githubusercontent.com/Prism-Network-io/default-token-list/master/logos/shared/empire.png",
     keywords: ["empire", "default"],
-    tokens: [...eth, ...bsc, ...ftm, ...avax, ...matic, ...xdai, ...cro, ...harmony]
+    tokens: [...eth, ...bsc, ...ftm, ...avax, ...matic, ...xdai, ...cro, ...harmony, ...kava]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
