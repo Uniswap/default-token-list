593e3de39b954726cbd800a90aaa2778348e09b4
matteenm <105068213+matteenm@users.noreply.github.com>
2022-06-23 18:42:47 -0400

    update list name (#1150)
    * update list name
    
    * Revert "4.0.0"
    
    This reverts commit 161879f58bed0c9d080165946f0659f8004239c6.

--- src/buildList.js
+++ src/buildList.js
@@ -11,7 +11,7 @@ const bridgeUtils = require('@uniswap/token-list-bridge-utils');
 module.exports = function buildList() {
   const parsed = version.split(".");
   const l1List = {
-    name: "Uniswap Labs Default List",
+    name: "Uniswap Labs Default",
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
