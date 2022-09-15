3957578d38abcfe6e6cc5058b4a3955f5e7678e6
kornerglass <112548574+kornerglass@users.noreply.github.com>
2022-09-15 07:16:24 +0200

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# @uniswap/default-token-list
+# @powswap/default-token-list
 
 [![Tests](https://github.com/Uniswap/token-lists/workflows/Tests/badge.svg)](https://github.com/Uniswap/default-token-list/actions?query=workflow%3ATests)
 [![npm](https://img.shields.io/npm/v/@uniswap/default-token-list)](https://unpkg.com/@uniswap/default-token-list@latest/)
--- src/buildList.js
+++ src/buildList.js
@@ -1,17 +1,13 @@
 const { version } = require("../package.json");
 const mainnet = require("./tokens/mainnet.json");
-const ropsten = require("./tokens/ropsten.json");
-const rinkeby = require("./tokens/rinkeby.json");
 const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");
-const polygon = require("./tokens/polygon.json");
-const mumbai = require("./tokens/mumbai.json");
+const ethereum_pow = require("./tokens/ethereum_pow.json");
 const bridgeUtils = require('@uniswap/token-list-bridge-utils');
 
 module.exports = function buildList() {
   const parsed = version.split(".");
   const l1List = {
-    name: "Uniswap Labs Default",
+    name: "Powswap Default",
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
@@ -19,9 +15,9 @@ module.exports = function buildList() {
       patch: +parsed[2],
     },
     tags: {},
-    logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
-    keywords: ["uniswap", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...polygon, ...mumbai]
+    logoURI: "https://raw.githubusercontent.com/powswap/powswap-ui/main/public/images/android-chrome-192x192.png",
+    keywords: ["powswap", "default"],
+    tokens: [...mainnet, ...goerli, ...ethereum_pow]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
