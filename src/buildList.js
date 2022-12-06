f65c4137dbf14ab47da29cc43314e17298ff95de
paralarımı çaldınız hepinizin anasını sikeyyiiim <104706466+Canti599@users.noreply.github.com>
2022-12-07 00:59:22 +0300

    Revert "Add CELO and some L2 Mappings (#1224)"
    This reverts commit 2d3f2c5991347daa7b240a0664b42e9af9a1ccbb.

--- src/buildList.js
+++ src/buildList.js
@@ -7,7 +7,6 @@ const kovan = require("./tokens/kovan.json");
 const polygon = require("./tokens/polygon.json");
 const mumbai = require("./tokens/mumbai.json");
 const optimism = require("./tokens/optimism.json");
-const celo = require("./tokens/celo.json");
 const bridgeUtils = require('@uniswap/token-list-bridge-utils');
 
 module.exports = function buildList() {
@@ -23,7 +22,7 @@ module.exports = function buildList() {
     tags: {},
     logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
     keywords: ["uniswap", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...polygon, ...mumbai, ...optimism, ...celo]
+    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...polygon, ...mumbai, ...optimism]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
