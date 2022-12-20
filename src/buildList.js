07b3a233c0b5215cc507cbf82996af42ebd5f624
Corey <corey@coreydavis.nz>
2022-12-20 20:24:18 +0000

    added empire to load

--- src/buildList.js
+++ src/buildList.js
@@ -9,27 +9,28 @@ const cro = require("./tokens/cro.json");
 const harmony = require("./tokens/harmony.json");
 const kava = require("./tokens/kava.json");
 const pom = require("./tokens/pom.json");
+const empire = require("./tokens/empire.json");
 
 module.exports = function buildList() {
-  const parsed = version.split(".");
-  return {
-    name: "Empire Default List",
-    timestamp: new Date().toISOString(),
-    version: {
-      major: +parsed[0],
-      minor: +parsed[1],
-      patch: +parsed[2],
-    },
-    tags: {},
-    logoURI: "https://raw.githubusercontent.com/Prism-Network-io/default-token-list/master/logos/shared/empire.png",
-    keywords: ["empire", "default"],
-    tokens: [...eth, ...bsc, ...ftm, ...avax, ...matic, ...xdai, ...cro, ...harmony, ...kava, ...pom]
-      // sort them by symbol for easy readability
-      .sort((t1, t2) => {
-        if (t1.chainId === t2.chainId) {
-          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
-        }
-        return t1.chainId < t2.chainId ? -1 : 1;
-      }),
-  };
+    const parsed = version.split(".");
+    return {
+        name: "Empire Default List",
+        timestamp: new Date().toISOString(),
+        version: {
+            major: +parsed[0],
+            minor: +parsed[1],
+            patch: +parsed[2],
+        },
+        tags: {},
+        logoURI: "https://raw.githubusercontent.com/Prism-Network-io/default-token-list/master/logos/shared/empire.png",
+        keywords: ["empire", "default"],
+        tokens: [...eth, ...bsc, ...ftm, ...avax, ...matic, ...xdai, ...cro, ...harmony, ...kava, ...pom, ...empire]
+            // sort them by symbol for easy readability
+            .sort((t1, t2) => {
+                if (t1.chainId === t2.chainId) {
+                    return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+                }
+                return t1.chainId < t2.chainId ? -1 : 1;
+            }),
+    };
 };
