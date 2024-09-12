ff893b4146bd14095927e78377c770dc46fc1855
matteen <matteen.mobasheran@uniswap.org>
2024-09-12 10:56:55 -0400

    Add cbbtc (#1860)
    * add cbBTC
    
    * 12.17.0
    
    * bump unit test timeout
    
    * bump test timeout more

--- test/uniswap-default.test.js
+++ test/uniswap-default.test.js
@@ -10,7 +10,7 @@ const validator = ajv.compile(schema);
 let defaultTokenList;
 
 before(async function () {
-  this.timeout(120000);
+  this.timeout(250000);
   defaultTokenList = await buildList();
 });
 
